a5b6bc29da05df9b2ea4acdb5a9b551c1213a20c
La Vesha Parker <ldp47@cornell.edu>
2015-03-02 23:52:39 -0500

    cleaning up the viz a bit

--- P1/js/graphThree.js
+++ P1/js/graphThree.js
@@ -1,7 +1,7 @@
 function plotGraphThree() {
                 var lineData = [{x:0 , y: 0}, {x:1 , y: 221125000}, {x:2 , y: 92500000}, {x:3 , y: 3607664440}, {x:4 , y: 138256000}, {x:5 , y: 699772134}, {x:6 , y: 34209000}, {x:7 , y: 44966798}, {x:8 , y: 873740000}, {x:9 , y: 5929050}, {x:10 , y: 4697703200}, {x:11 , y: 0}];
                 var vis = d3.select('#money'),
-                WIDTH = 650,
+                WIDTH = 800,
                 HEIGHT = 500,
                 MARGINS = {
                   top: 40,
